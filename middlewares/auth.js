d7eb75bfc01f46f2a6d91fbe1e39e4970eac8504
Sayed Mubarish <mubarish007@gmail.com>
2025-07-16 14:47:23 +0530

    change body parser version

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,11 +5,8 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
     const userId = decodedToken.userId;
-    console.log("token",token);
-    console.log(req.body);
-    
-    
-    if (req.body && req.body.userId && req.body.userId !== userId) {
+   
+    if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
     } else {
       next();
